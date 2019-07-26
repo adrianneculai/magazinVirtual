b56b1ad89eecafa5af55d262e714cb973c09ee87
adrianneculai <adrian.razvan83@gmail.com>
2019-07-26 03:42:35 +0300

    penultia oara

--- script.js
+++ script.js
@@ -43,18 +43,20 @@ async function getObj(param) {
 
 function draw() {
     var content = ""
-
     for (var i in pokemon) {
         content += `
         <div class="produseWrap">
         <div class="produse">
+        
             <img src="${pokemon[i].scr}}" alt="">
             <h3>${pokemon[i].name} <br>
             </h3>
             <h3>${pokemon[i].price} $</h3>
+
             <button id="btnCumpUnProdus" onclick="adaugaInCos('${i}')">Cumpara</button>
             <button onclick="detaliiProba('${i}')"  type="button">Detalii</button>
         </div>
+        <div class="container ${pokemon[i].tip}" style="position:absolute;"></div>
     </div>
                 `
     }
@@ -420,7 +422,7 @@ function drawCeAiCumparat() {
 function drawCarousel() {
 
 
-    
+
     let content = ""
     let arrayForCarousel = []
 
@@ -490,101 +492,149 @@ function drawCarousel() {
 
 
 
-function carouselBehaviour(){
+function carouselBehaviour() {
 
-    
-const track = document.querySelector('.carousel__track')
-const slides = Array.from(track.children)
 
-const nextButton = document.querySelector(".carousel__button--right")
-const prevButton = document.querySelector(".carousel__button--left")
-const dotsNav = document.querySelector(".carousel__nav")
-const dots = Array.from(dotsNav.children)
+    const track = document.querySelector('.carousel__track')
+    const slides = Array.from(track.children)
 
-const slideWidth = slides[0].getBoundingClientRect().width
+    const nextButton = document.querySelector(".carousel__button--right")
+    const prevButton = document.querySelector(".carousel__button--left")
+    const dotsNav = document.querySelector(".carousel__nav")
+    const dots = Array.from(dotsNav.children)
 
-// console.log(slideSize)
-// slides[0].style.left = slideWidth * 0 + "px"
-// slides[1].style.left = slideWidth * 1 + "px"
-// slides[2].style.left = slideWidth * 2 + "px"
+    const slideWidth = slides[0].getBoundingClientRect().width
 
-const setSlidePosition = (slide, index) => {
-    slide.style.left = slideWidth * index + "px"
+    // console.log(slideSize)
+    // slides[0].style.left = slideWidth * 0 + "px"
+    // slides[1].style.left = slideWidth * 1 + "px"
+    // slides[2].style.left = slideWidth * 2 + "px"
+
+    const setSlidePosition = (slide, index) => {
+        slide.style.left = slideWidth * index + "px"
+    }
+    slides.forEach(setSlidePosition)
+
+
+    const moveToSlide = (track, currentSlide, targetSlide) => {
+        track.style.transform = 'translatex(-' + targetSlide.style.left + ')'
+        currentSlide.classList.remove('current-slide')
+        targetSlide.classList.add('current-slide')
+    }
+
+
+    const updateDots = (currentDot, targetDot) => {
+        currentDot.classList.remove('current-slide')
+        targetDot.classList.add('current-slide')
+    }
+
+
+    const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+        if (targetIndex === 0) {
+            prevButton.classList.add('is-hidden')
+            nextButton.classList.remove('is-hidden')
+        } else if (targetIndex === slides.length - 1) {
+            nextButton.classList.add('is-hidden')
+            prevButton.classList.remove('is-hidden')
+        } else {
+            prevButton.classList.remove('is-hidden')
+            nextButton.classList.remove('is-hidden')
+        }
+    }
+
+
+    prevButton.addEventListener("click", e => {
+        const currentSlide = track.querySelector(".current-slide")
+        const prevSlide = currentSlide.previousElementSibling
+        const currentDot = dotsNav.querySelector('.current-slide')
+        const prevDot = currentDot.previousElementSibling
+        const prevIndex = slides.findIndex(slide => slide === prevSlide)
+
+
+        moveToSlide(track, currentSlide, prevSlide)
+        updateDots(currentDot, prevDot)
+        hideShowArrows(slides, prevButton, nextButton, prevIndex)
+
+    })
+
+
+    nextButton.addEventListener("click", e => {
+        const currentSlide = track.querySelector(".current-slide")
+        const nextSlide = currentSlide.nextElementSibling
+        const currentDot = dotsNav.querySelector('.current-slide')
+        const nextDot = currentDot.nextElementSibling
+        const nextIndex = slides.findIndex(slide => slide === nextSlide)
+
+        moveToSlide(track, currentSlide, nextSlide)
+        updateDots(currentDot, nextDot)
+        hideShowArrows(slides, prevButton, nextButton, nextIndex)
+
+    })
+
+    dotsNav.addEventListener('click', e => {
+        const targetDot = e.target.closest('button')
+
+        if (!targetDot) return;
+
+        const currentSlide = track.querySelector('.current-slide')
+        const currentDot = dotsNav.querySelector('.current-slide')
+        const targetIndex = dots.findIndex(dot => dot === targetDot)
+        const targetSlide = slides[targetIndex]
+
+        moveToSlide(track, currentSlide, targetSlide)
+        updateDots(currentDot, targetDot)
+        hideShowArrows(slides, prevButton, nextButton, targetIndex)
+    })
+
+    produseInCos()
 }
-slides.forEach(setSlidePosition)
 
 
-const moveToSlide = (track, currentSlide, targetSlide) => {
-    track.style.transform = 'translatex(-' + targetSlide.style.left + ')'
-    currentSlide.classList.remove('current-slide')
-    targetSlide.classList.add('current-slide')
-}
+function searchPoke() {
 
+    var input = document.getElementById('search')
+    var filter = input.value.toLowerCase()
+    var list = document.querySelectorAll(".produse:first-child h3:first-of-type")
 
-const updateDots = (currentDot, targetDot) => {
-    currentDot.classList.remove('current-slide')
-    targetDot.classList.add('current-slide')
-}
+    console.log(list)
+    console.log('list de 2 ar trbuisa fie pikachu: ' + list[2].textContent.toLocaleLowerCase())
+    console.log(filter)
 
+    console.log(list[2].textContent.toLocaleLowerCase().indexOf(filter))
 
-const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
-    if (targetIndex === 0) {
-        prevButton.classList.add('is-hidden')
-        nextButton.classList.remove('is-hidden')
-    } else if (targetIndex === slides.length - 1) {
-        nextButton.classList.add('is-hidden')
-        prevButton.classList.remove('is-hidden')
-    } else {
-        prevButton.classList.remove('is-hidden')
-        nextButton.classList.remove('is-hidden')
+    for (var i in list) {
+        if (list[i].textContent.toLocaleLowerCase().indexOf(filter) == -1) {
+            list[i].parentElement.parentElement.classList.add('hide')
+        } else {
+            list[i].parentElement.parentElement.classList.remove('hide')
+        }
     }
 }
 
 
-prevButton.addEventListener("click", e => {
-    const currentSlide = track.querySelector(".current-slide")
-    const prevSlide = currentSlide.previousElementSibling
-    const currentDot = dotsNav.querySelector('.current-slide')
-    const prevDot = currentDot.previousElementSibling
-    const prevIndex = slides.findIndex(slide => slide === prevSlide)
+function cautaTip() {
+    var tipSearch = document.querySelector("[name=\"tipPokemon\"]").value
+    console.log("este ce caut eu: " + tipSearch)
+    var list = document.querySelectorAll(".produseWrap div:nth-child(2)")
+    console.log(list[1].classList[1])
+    console.log(list[0].parentElement.classList.add('hide'))
+
+    for (var i in list) {
+        // console.log(list[i].classList[1])
+        // console.log("acesta este tipul pe care il caut: " + tipSearch)
+        // console.log(list[i].classList[1] != tipSearch)
 
 
-    moveToSlide(track, currentSlide, prevSlide)
-    updateDots(currentDot, prevDot)
-    hideShowArrows(slides, prevButton, nextButton, prevIndex)
-
-})
-
-
-nextButton.addEventListener("click", e => {
-    const currentSlide = track.querySelector(".current-slide")
-    const nextSlide = currentSlide.nextElementSibling
-    const currentDot = dotsNav.querySelector('.current-slide')
-    const nextDot = currentDot.nextElementSibling
-    const nextIndex = slides.findIndex(slide => slide === nextSlide)
-
-    moveToSlide(track, currentSlide, nextSlide)
-    updateDots(currentDot, nextDot)
-    hideShowArrows(slides, prevButton, nextButton, nextIndex)
-
-})
-
-dotsNav.addEventListener('click', e => {
-    const targetDot = e.target.closest('button')
-
-    if (!targetDot) return;
-
-    const currentSlide = track.querySelector('.current-slide')
-    const currentDot = dotsNav.querySelector('.current-slide')
-    const targetIndex = dots.findIndex(dot => dot === targetDot)
-    const targetSlide = slides[targetIndex]
-
-    moveToSlide(track, currentSlide, targetSlide)
-    updateDots(currentDot, targetDot)
-    hideShowArrows(slides, prevButton, nextButton, targetIndex)
-})
+        if (list[i].classList[1] != tipSearch && tipSearch != 0) {
+            list[i].parentElement.classList.add('hide')
+        } else {
+            list[i].parentElement.classList.remove('hide')
+        }
 
 
+        // if (tipSearch === 0) {
+        //     document.querySelector(".produseWrap hide").className = "produseWrap"
+        // }
 
+    }
 }
-
